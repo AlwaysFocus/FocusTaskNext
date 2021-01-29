a7a8d5bbd5489fa48a144c65097e39cbc8178550
AlwaysFocus <eliwhalen3@gmail.com>
2021-01-28 22:24:40 -0700

    first commit

---
+++ pages/projects/github-search.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+export default function GithubSearch()
+{
+    
+
+    return(
+        <div>
+        <article className="bg-green-300"> Hello!</article>
+        <Link href="/">
+            <a>
+                Go Home
+            </a>
+        </Link>
+        </div>
+    )
+}
